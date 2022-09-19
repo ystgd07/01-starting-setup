8584412c833ba7c11a3d098507888be5c56bc724
yangsungsoo <ysotgood@gmail.com>
2022-09-19 18:31:08 +0900

    js_props_components_3

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 9, 8);
   // const expeneseTitle = "Car Insurance";
@@ -10,13 +11,13 @@ function ExpenseItem(props) {
   // const day = props.date.toLocaleString("ko-KR", { day: "numeric" });
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount * 1200}원</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
